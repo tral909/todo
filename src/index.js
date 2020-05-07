fbf3ca78944e314bb1a7b484213b124fc97efeb0
tral909 <indorm1992@mail.ru>
2020-05-07 12:54:15 +0300

    JSX, try curly braces evaluation

--- src/index.js
+++ src/index.js
@@ -2,21 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
+
+  const items = ['Drink Coffee', 'Build Awesome App']  
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
     </ul>
   );
 }
 
 const AppHeader = () => { return <h1>My Todo List</h1> }
 
-const SearchPanel = () => { return <input placeholder="search" /> }
+const SearchPanel = () => {
+
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '20px'
+  };
+
+  return <input style={searchStyle} placeholder={searchText} />
+}
 
 const App = () => {
+
+  const isLoggedIn = true;
+  const loginBox = <span>Log in please</span>;
+  const welcomBox = <span>Welcome Back</span>;
+  //xss protection
+  const value = '<script>alert("")</script>';
   return (
     <div>
+      { value }
+      { isLoggedIn ? welcomBox : loginBox }
       <AppHeader />
       <SearchPanel />
       <TodoList />
