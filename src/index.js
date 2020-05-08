ceaf995770952dde6eeed85b52b85c31985982e9
tral909 <indorm1992@mail.ru>
2020-05-08 19:08:57 +0300

    Divide components by files

--- src/index.js
+++ src/index.js
@@ -1,40 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-
-  const items = ['Drink Coffee', 'Build Awesome App']  
-  return (
-    <ul>
-      <li>{ items[0] }</li>
-      <li>{ items[1] }</li>
-    </ul>
-  );
-}
-
-const AppHeader = () => { return <h1>My Todo List</h1> }
-
-const SearchPanel = () => {
-
-  const searchText = 'Type here to search';
-  const searchStyle = {
-    fontSize: '20px'
-  };
-
-  return <input style={searchStyle} placeholder={searchText} />
-}
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-
-  const isLoggedIn = true;
-  const loginBox = <span>Log in please</span>;
-  const welcomBox = <span>Welcome Back</span>;
-  //xss protection
-  const value = '<script>alert("")</script>';
   return (
     <div>
-      { value }
-      { isLoggedIn ? welcomBox : loginBox }
       <AppHeader />
       <SearchPanel />
       <TodoList />
