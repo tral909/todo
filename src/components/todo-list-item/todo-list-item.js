32f4023ffabcc901a26d33607a3a7ec7cde33cf3
tral909 <indorm1992@mail.ru>
2020-05-10 19:07:35 +0300

    Use setState with object and func param

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,29 +4,51 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  onLabelClick = () => {
-    console.log(`Done ${this.props.label}`)
+  state = {
+    done: false,
+    important: false
   };
 
-  render() {
-    const { label, important = false } = this.props;
+  onLabelClick = () => {
+    this.setState(({ done }) => {
+      return {
+        done: !done
+      }
+    });
+  };
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        important: !state.important
+      }
+    });
+  }
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
   
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={ this.onLabelClick }>
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={ this.onMarkImportant }>
           <i className="fa fa-exclamation" />
         </button>
   
