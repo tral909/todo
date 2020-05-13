78aba2ee446fad7eba24cc18113984dcfd51458d
tral909 <indorm1992@mail.ru>
2020-05-13 09:26:51 +0300

    fix add button text

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -31,7 +31,7 @@ export default class ItemAddForm extends Component {
           placeholder="What needs to be done"
           value={this.state.label} />
         <button
-          className="btn btn-outline-secondary">
+          className="btn btn-outline-secondary text-nowrap">
           Add Item
         </button>
       </form>
